e7232405e13a9cde2c48ee63fe3bf58bdc8ff3ac
Penko Dimitrov <111526654+Penqka@users.noreply.github.com>
2023-08-04 22:48:04 +0300

    add sub part 2

---
+++ sub/test.pm
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+
+# sub test ($) {
+#     # my $arr = shift;
+#     # print $_ for @$arr;
+#     print $_ for @{$_[0]};
+# }
+
+# test([1,2,3]);
+
+
+
+# sub test_arr (\@\@) {
+#     my ($a, $b) = @_;
+#     print @$a, "\n";
+#     print @$b, "\n";
+
+# }
+
+# my @c = (1,2,3);
+# my @d = (4,5,6);
+
+# test_arr(@c, @d);
+
+sub or_test {
+    $a = shift || "nothing";
+    print $a;
+}
+
+
+
+my $ref = \&or_test;
+
+$ref->('putka');
